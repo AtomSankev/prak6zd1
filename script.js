56aa5c400f480913964715dcea4073af8ea78dc1
AtomSankev <159898361+AtomSankev@users.noreply.github.com>
2024-06-05 22:39:05 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function changeBackgroundColor() {
+    const colorSelect = document.getElementById('colorSelect');
+    const selectedColor = colorSelect.value;
+    document.body.style.backgroundColor = selectedColor;
+}
